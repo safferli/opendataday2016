c7db2f054ab713862f323136c74fd69411c1d104
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-03-05 12:29:20 +0100

    Ãstreetnames in csv for quick and dirty

--- streets.R
+++ streets.R
@@ -18,9 +18,8 @@ setwd(wd)
 
 # http://offenedaten.frankfurt.de/dataset/strassenverzeichnis-der-stadt-frankfurt-am-main
 # http://www.frankfurt.de/sixcms/detail.php?id=2911
-
 ffm <- "http://www.offenedaten.frankfurt.de/dataset/7bd9de80-98de-45c2-99a8-497ce7b3c82c/resource/be5982fe-ed79-42f4-acdc-57ca4737fb7a/download/strassenverzeichnis2014.csv"
-
+# data is encoded in windows from the city... 
 dta <- read.csv(ffm, sep=";", stringsAsFactors = FALSE, fileEncoding = "WINDOWS-1258")
 
 streets <- dta %>% 
@@ -32,3 +31,6 @@ districts <- streets %>%
   group_by(Stadtteil.Name) %>% 
   tally() %>% 
   arrange(-n)
+
+# write csv of streetnames to get started on the nodejs side
+write.csv(unique(streets$Straßenname), file = "ffm-streetnames.csv", row.names = FALSE)
