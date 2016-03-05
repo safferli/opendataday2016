0f269ea739205e211bd9f95bff9e94e29cdd5aec
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-03-05 11:48:56 +0100

    district aggregation

--- streets.R
+++ streets.R
@@ -21,4 +21,14 @@ setwd(wd)
 
 ffm <- "http://www.offenedaten.frankfurt.de/dataset/7bd9de80-98de-45c2-99a8-497ce7b3c82c/resource/be5982fe-ed79-42f4-acdc-57ca4737fb7a/download/strassenverzeichnis2014.csv"
 
-streets <- read.csv(ffm, sep=";")
+dta <- read.csv(ffm, sep=";", stringsAsFactors = FALSE)
+
+streets <- dta %>% 
+  select(Straßenname, Folge, Hausnr...von., Hausnr...bis., Stadtteil.Name, Postleitzahl)
+
+districts <- streets %>% 
+  # group_by(Straßenname) %>% 
+  # ungroup() %>% 
+  group_by(Stadtteil.Name) %>% 
+  tally() %>% 
+  arrange(-n)
