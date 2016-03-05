f78d58cf76ba4b88d1088b42e17be074badf55b7
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-03-05 11:37:23 +0100

    read street list from FFM open data portal

---
+++ streets.R
@@ -0,0 +1,24 @@
+rm(list = ls()); gc(); gc()
+# options(java.parameters = "-Xmx4096m")#8192
+# options(java.parameters = "-XX:-UseConcMarkSweepGC")
+# options(java.parameters = "-XX:-UseGCOverheadLimit")
+options(bitmapType='cairo')
+options(scipen = 999)
+#library(bbbi)
+library(ggplot2)
+#library(RODBC)
+library(dplyr)
+#library(tidyr)
+#library(data.table)
+
+# Define your workspace: "X:/xxx/"
+wd <- "D:/github/opendataday2016/"
+#wd <- "/home/csafferling/Documents/github/statistics101"
+setwd(wd)
+
+# http://offenedaten.frankfurt.de/dataset/strassenverzeichnis-der-stadt-frankfurt-am-main
+# http://www.frankfurt.de/sixcms/detail.php?id=2911
+
+ffm <- "http://www.offenedaten.frankfurt.de/dataset/7bd9de80-98de-45c2-99a8-497ce7b3c82c/resource/be5982fe-ed79-42f4-acdc-57ca4737fb7a/download/strassenverzeichnis2014.csv"
+
+streets <- read.csv(ffm, sep=";")
